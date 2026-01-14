42f7a00327ff43b7861cf29dd77507ef05971dd0
Mason Bickers <masonbickers@MacBook-Pro.local>
2026-01-14 09:55:28 +0000

    Add Klaviyo onsite tracking + track waitlist signup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,10 +25,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+
+        {/* ✅ Klaviyo onsite tracking (loads on every page) */}
+        <Script
+          async
+          src="https://static.klaviyo.com/onsite/js/S55cAn/klaviyo.js?company_id=S55cAn"
+          strategy="afterInteractive"
+        />
+
+        <Script id="klaviyo-init" strategy="afterInteractive">
+          {`
+            !function(){if(!window.klaviyo){window._klOnsite=window._klOnsite||[];
+              try{window.klaviyo=new Proxy({},{get:function(n,i){
+                return"push"===i?function(){var n;(n=window._klOnsite).push.apply(n,arguments)}:
+                function(){for(var n=arguments.length,o=new Array(n),w=0;w<n;w++)o[w]=arguments[w];
+                  var t="function"==typeof o[o.length-1]?o.pop():void 0,
+                  e=new Promise((function(n){window._klOnsite.push([i].concat(o,[function(i){t&&t(i),n(i)}]))}));
+                  return e}}})}
+              catch(n){window.klaviyo=window.klaviyo||[],window.klaviyo.push=function(){var n;(n=window._klOnsite).push.apply(n,arguments)}}}}();
+          `}
+        </Script>
       </body>
     </html>
   );
